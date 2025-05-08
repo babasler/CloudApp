ac59250f50f00e5a2765e2745ebcea111d4d9410
Bastian Basler <bastian.basler2002@gmail.com>
2025-05-08 23:31:38 +0200

    add all components to translate Text

--- SmartTranslateFrontend/src/app/translator-component/translator-component.component.ts
+++ SmartTranslateFrontend/src/app/translator-component/translator-component.component.ts
@@ -1,32 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SelectModule } from 'primeng/select';
+import { TextareaModule } from 'primeng/textarea';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
-interface City {
+interface language {
   name: string;
   code: string;
 }
 
 @Component({
   selector: 'translator-component',
-  imports: [FormsModule, SelectModule],
+  imports: [FormsModule, SelectModule, TextareaModule, IftaLabelModule, ButtonModule, CardModule],
   templateUrl: './translator-component.component.html',
   styleUrl: './translator-component.component.scss',
   standalone: true
 })
 export class TranslatorComponentComponent implements OnInit {
-  cities: City[] | undefined;
+  possibleLanguages: language[] = [];
+  sourceLanguage: language = { name: '', code: '' };
+  targetLanguage: language = { name: '', code: '' };
+  sourceText:string = '';
+  targetText:string = '';
 
-    selectedCity: City | undefined;
+  ngOnInit() {
+    this.possibleLanguages = [
+      { name: 'Deutsch', code: 'de' },
+      { name: 'Englisch', code: 'en' },
+      { name: 'Spanisch', code: 'es' },
+      { name: 'Schwedisch', code: 's' },
+      { name: 'Französisch', code: 'fr' } // TODO: Hier mit Restabfrage arbeiten
+    ];
+  }
 
-    ngOnInit() {
-        this.cities = [
-            { name: 'Deutsch', code: 'de' },
-            { name: 'Englisch', code: 'en' },
-            { name: 'Spanisch', code: 'es' },
-            { name: 'Schwedisch', code: 's' },
-            { name: 'Französisch', code: 'fr' }
-        ];
-    }
+  canTranslate(): boolean {
+    return this.sourceLanguage.name.length > 0 && this.targetLanguage.name.length > 0&& this.sourceText.length > 0;
+  }
+  translate(): void {
+    this.targetText = "Hallo Welt"; // TODO: Hier mit Restabfrage arbeiten
+  }
 
 }
